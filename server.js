696224759de722cb972540f573868eb8ed2791dd
prathmkhedkar63-oss <prathmkhedkar63@gmail.com>
2026-08-28 03:36:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,17 +9,14 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// Root route → index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// WebRTC signaling
 io.on("connection", (socket) => {
-  console.log("New client connected:", socket.id);
+  console.log("Client connected:", socket.id);
 
   socket.on("offer", ({ offer }) => {
     socket.broadcast.emit("offer", { offer, clientId: socket.id });
@@ -27,10 +24,6 @@ io.on("connection", (socket) => {
 
   socket.on("answer", ({ answer, clientId }) => {
     io.to(clientId).emit("answer", answer);
-  });
-
-  socket.on("join-admin", () => {
-    console.log("Admin joined");
   });
 
   socket.on("disconnect", () => {
